9c35caa02a32990bafca1b9c09171f9de0e09941
Nigel Fang <nigel.fang00@gmail.com>
2019-12-18 01:18:40 -0800

    first commit

---
+++ Config.py
@@ -0,0 +1,10 @@
+
+# Courses are a list of CRN numbers so change that according to the CRN you want to enroll. Seperate CRNs by '  ' and a comma.
+
+CONFIG = 
+{
+	'USERNAME_STR' : 'Insert username here'
+	'PASSWORD_STR' : 'Insert password here'
+	'COURSES' : ['Course 1', 'Course 2' , ....]
+}
+
